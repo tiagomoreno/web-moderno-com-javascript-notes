ae4262a5a81d1f16ba534db8ce7311bae3b92ab6
Tiago Moreno <tiagohrm@gmail.com>
2020-12-25 17:29:24 -0300

    Estudos em reduce

---
+++ array/reduce3-simulacao.js
@@ -0,0 +1,16 @@
+//Simulação simplificada de implementação de Reduce
+//Aula do curso como refêrencia - 118
+Array.prototype.reduce2 = function(callback, valorInicial){
+    const indiceInicial = valorInicial ? 0 : 1
+    let acumulador = valorInicial || this[0]
+    for (let i = indiceInicial; i < this.length; i++ ){
+        acumulador = callback(acumulador, this[i], i, this)
+    }
+    return acumulador
+}
+
+    
+    const soma = (total, valor) => total + valor
+    const nums = [1,2,3,4,5,6]
+    console.log(nums.reduce2(soma,21))
+
