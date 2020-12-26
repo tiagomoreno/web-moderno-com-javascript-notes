0c6a392c05e18a7a9003edfec5a8173442b87020
Tiago Moreno <tiagohrm@gmail.com>
2020-12-26 16:23:22 -0300

    Estudos com Array

---
+++ array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Ualeskah', 'Cibalena']
+const filhos = ['Uoxiton', 'Uesclei']
+
+const todos = filhas.concat(filhos, filhas)
+console.log(todos, filhos, filhas)
+
+console.log([].concat([1,2],[3,4],5,[[6,7]]))
